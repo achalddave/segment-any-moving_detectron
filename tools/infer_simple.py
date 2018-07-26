9b04b4145731be083828dd03df929c06d39bea6d
Achal Dave <achalddave@live.com>
2018-07-25 21:51:07 -0400

    Log exceptions from infer_simple

--- tools/infer_simple.py
+++ tools/infer_simple.py
@@ -257,4 +257,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as e:
+        logging.exception('Caught exception: %s', e)
+        sys.exit(1)
