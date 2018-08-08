72ff921ba4885eeccb2db79caedca5d17aea1b9d
Achal Dave <achalddave@live.com>
2018-08-08 14:27:58 -0400

    Use utils.setup_logging, add progress bar to infer_simple

--- tools/infer_simple.py
+++ tools/infer_simple.py
@@ -10,6 +10,7 @@ import pickle
 import sys
 import subprocess
 from collections import defaultdict
+from datetime import datetime
 from pathlib import Path
 from pprint import pformat
 from six.moves import xrange
@@ -19,6 +20,7 @@ import matplotlib ; matplotlib.use('Agg')
 
 import numpy as np
 import cv2
+from tqdm import tqdm
 
 import torch
 import torch.nn as nn
@@ -35,6 +37,7 @@ import utils.net as net_utils
 import utils.vis as vis_utils
 from utils.detectron_weight_helper import load_detectron_weight
 from utils.flow import load_flow_png
+from utils.logging import setup_logging
 from utils.timer import Timer
 
 # OpenCL may be enabled by default in OpenCV3; disable it because it's not
@@ -86,44 +89,6 @@ def parse_args():
     return args
 
 
-def _set_logging(logging_filepath):
-    """Setup root logger to log to file and stdout.
-
-    All calls to logging will log to `logging_filepath` as well as stdout.
-    Also creates a file logger that only logs to , which can
-    be retrieved with logging.getLogger(logging_filepath).
-
-    Args:
-        logging_filepath (str): Path to log to.
-    """
-    log_format = ('%(asctime)s %(filename)s:%(lineno)4d: ' '%(message)s')
-    stream_date_format = '%H:%M:%S'
-    file_date_format = '%m/%d %H:%M:%S'
-
-    # Clear any previous changes to logging.
-    logging.root.handlers = []
-    logging.root.setLevel(logging.INFO)
-
-    file_handler = logging.FileHandler(logging_filepath)
-    file_handler.setFormatter(
-        logging.Formatter(log_format, datefmt=file_date_format))
-    logging.root.addHandler(file_handler)
-
-    # Logger that logs only to file. We could also do this with levels, but
-    # this allows logging specific messages regardless of level to the file
-    # only (e.g. to save the diff of the current file to the log file).
-    file_logger = logging.getLogger(logging_filepath)
-    file_logger.addHandler(file_handler)
-    file_logger.propagate = False
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(
-        logging.Formatter(log_format, datefmt=stream_date_format))
-    logging.root.addHandler(console_handler)
-
-    logging.info('Writing log file to %s', logging_filepath)
-
-
 def is_image(path):
     return path.is_file and any(path.suffix == extension
                                 for extension in misc_utils.IMG_EXTENSIONS)
@@ -137,9 +102,10 @@ def main():
 
     args = parse_args()
     output_dir = Path(args.output_dir)
-    logging_path = str(output_dir / 'detectron-pytorch.log')
+    logging_path = str(output_dir / (
+        'detectron-pytorch-%s.log' % datetime.now().strftime('%b%d-%H-%M-%S')))
     output_dir.mkdir(parents=True, exist_ok=True)
-    _set_logging(logging_path)
+    setup_logging(logging_path)
 
     file_logger = logging.getLogger(logging_path)
     file_logger.info('Source code:')
@@ -215,7 +181,7 @@ def main():
     output_pickles = [x.with_suffix('.pickle') for x in output_images]
 
     for image_path, out_image, out_data in zip(
-            images, output_images, output_pickles):
+            tqdm(images), output_images, output_pickles):
         if input_is_flow:
             im = load_flow_png(str(image_path))
         else:
@@ -224,14 +190,15 @@ def main():
 
         if ((not args.save_images or os.path.isfile(out_image))
                 and os.path.isfile(out_data)):
-            logging.info('Already processed {}, skipping'.format(image_path))
+            file_logger.info(
+                'Already processed {}, skipping'.format(image_path))
             continue
         timers = defaultdict(Timer)
 
         cls_boxes, cls_segms, cls_keyps = im_detect_all(
             maskRCNN, im, timers=timers)
 
-        logging.info('Processing {} -> {}'.format(
+        file_logger.info('Processing {} -> {}'.format(
             image_path, out_image if args.save_images else out_data))
 
         if args.save_images and not os.path.isfile(out_image):
