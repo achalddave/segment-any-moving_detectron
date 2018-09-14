achalddave/segment-any-moving_detectron

commit 88b4809f32261d8eed4367d95f71473818308808

Implement testing with stacked frames

lib/utils/blob.py
@@ -122,7 +122,8 @@ def prep_im_for_blob(im, pixel_means, target_sizes, max_size):
     the scale factors that were used to compute each returned image.
     """
     im = im.astype(np.float32, copy=False)
-    im -= pixel_means
+    im_list = unpack_sequence(im)
+    im = pack_sequence([x - pixel_means for x in im_list])
     im_shape = im.shape
     im_size_min = np.min(im_shape[0:2])
     im_size_max = np.max(im_shape[0:2])
@@ -131,9 +132,16 @@ def prep_im_for_blob(im, pixel_means, target_sizes, max_size):
     im_scales = []
     for target_size in target_sizes:
         im_scale = get_target_scale(im_size_min, im_size_max, target_size, max_size)
-        im_resized = cv2.resize(im, None, None, fx=im_scale, fy=im_scale,
-                                interpolation=cv2.INTER_LINEAR)
-        ims.append(im_resized)
+        im_resized = [
+            cv2.resize(
+                x,
+                None,
+                None,
+                fx=im_scale,
+                fy=im_scale,
+                interpolation=cv2.INTER_LINEAR) for x in im_list
+        ]
+        ims.append(pack_sequence(im_resized))
         im_scales.append(im_scale)
     return ims, im_scales
 
@@ -186,3 +194,57 @@ def deserialize(arr):
     a workspace. See serialize().
     """
     return pickle.loads(arr.astype(np.uint8).tobytes())
+
+
+def unpack_sequence(sequence_concat):
+    """
+    Args:
+        sequence_concat (ndarray): Shape (height, width, 3 * num_images)
+
+    Returns:
+        list of ndarray with num_images elements, each of shape
+            (height, width, 3).
+
+    >>> x = np.random.rand(50, 50, 9)
+    >>> x_out = unpack_sequence(x)
+    >>> np.array_equal(x_out[0], x[:, :, :3])
+    True
+    >>> np.array_equal(x_out[1], x[:, :, 3:6])
+    True
+    >>> np.array_equal(x_out[2], x[:, :, 6:9])
+    True
+    >>> np.array_equal(pack_sequence(x_out), x)
+    True
+
+    >>> x = np.random.rand(50, 50, 3)
+    >>> x_out = unpack_sequence(x)
+    >>> len(x_out)
+    1
+    >>> np.array_equal(x, x_out[0])
+    True
+    """
+    assert sequence_concat.shape[2] % 3 == 0, (
+        'Unexpected number of channels: %s' % sequence_concat.shape[2])
+    num_images = sequence_concat.shape[2] / 3
+    return np.split(sequence_concat, num_images, axis=2)
+
+
+def pack_sequence(sequence_list):
+    """
+    Args:
+        sequence_list (list of ndarray): num_images elements, each of
+            shape (height, width, 3).
+
+    Returns:
+        ndarray: Shape (height, width, 3 * num_images)
+
+    >>> x = [np.random.rand(50, 50, 3) for _ in range(3)]
+    >>> x_out = pack_sequence(x)
+    >>> np.array_equal(x[0], x_out[:, :, :3])
+    True
+    >>> np.array_equal(x[1], x_out[:, :, 3:6])
+    True
+    >>> np.array_equal(x[2], x_out[:, :, 6:9])
+    True
+    """
+    return np.concatenate(sequence_list, axis=2)

lib/core/test.py
@@ -49,15 +49,13 @@ import utils.keypoints as keypoint_utils
 
 def im_detect_all(model, im, box_proposals=None, timers=None):
     """Process the outputs of model for testing
+
     Args:
-      model: the network module
-      im_data: Pytorch variable. Input batch to the model.
-      im_info: Pytorch variable. Input batch to the model.
-      gt_boxes: Pytorch variable. Input batch to the model.
-      num_boxes: Pytorch variable. Input batch to the model.
-      args: arguments from command line.
-      timer: record the cost of time for different steps
-    The rest of inputs are of type pytorch Variables and either input to or output from the model.
+        model: the network module
+        im (np.ndarray): Shape (height, width, num_channels). If working with
+          stacked optical flow, num_channels = 3 * NUM_STACKED_FRAMES.
+        box_proposals
+        timer: record the cost of time for different steps
     """
     if timers is None:
         timers = defaultdict(Timer)
@@ -174,7 +172,7 @@ def im_detect_bbox(model, im, target_scale, target_max_size, boxes=None):
             box_deltas = box_deltas.view(-1, 4) * cfg.TRAIN.BBOX_NORMALIZE_STDS \
                          + cfg.TRAIN.BBOX_NORMALIZE_MEANS
         pred_boxes = box_utils.bbox_transform(boxes, box_deltas, cfg.MODEL.BBOX_REG_WEIGHTS)
-        pred_boxes = box_utils.clip_tiled_boxes(pred_boxes, im.shape)
+        pred_boxes = box_utils.clip_tiled_boxes(pred_boxes, im.shape[:2])
         if cfg.MODEL.CLS_AGNOSTIC_BBOX_REG:
             pred_boxes = np.tile(pred_boxes, (1, scores.shape[1]))
     else:
@@ -332,7 +330,10 @@ def im_detect_bbox_aspect_ratio(
     Returns predictions in the original image space.
     """
     # Compute predictions on the transformed image
-    im_ar = image_utils.aspect_ratio_rel(im, aspect_ratio)
+    im_ar = blob_utils.pack_sequence([
+        image_utils.aspect_ratio_rel(x, aspect_ratio)
+        for x in blob_utils.unpack_sequence(im)
+    ])
 
     if not cfg.MODEL.FASTER_RCNN:
         box_proposals_ar = box_utils.aspect_ratio(box_proposals, aspect_ratio)
@@ -509,7 +510,10 @@ def im_detect_mask_aspect_ratio(model, im, aspect_ratio, boxes, hflip=False):
     """Computes mask detections at the given width-relative aspect ratio."""
 
     # Perform mask detection on the transformed image
-    im_ar = image_utils.aspect_ratio_rel(im, aspect_ratio)
+    im_ar = blob_utils.pack_sequence([
+        image_utils.aspect_ratio_rel(x, aspect_ratio)
+        for x in blob_utils.unpack_sequence(im)
+    ])
     boxes_ar = box_utils.aspect_ratio(boxes, aspect_ratio)
 
     if hflip:
@@ -685,7 +689,10 @@ def im_detect_keypoints_aspect_ratio(
     """Detects keypoints at the given width-relative aspect ratio."""
 
     # Perform keypoint detectionon the transformed image
-    im_ar = image_utils.aspect_ratio_rel(im, aspect_ratio)
+    im_ar = blob_utils.pack_sequence([
+        image_utils.aspect_ratio_rel(x, aspect_ratio)
+        for x in blob_utils.unpack_sequence(im)
+    ])
     boxes_ar = box_utils.aspect_ratio(boxes, aspect_ratio)
 
     if hflip:

lib/core/test_engine.py
@@ -43,6 +43,7 @@ import utils.env as envu
 import utils.net as net_utils
 import utils.vis as vis_utils
 from utils import subprocess_utils
+from utils.blob import pack_sequence
 from utils.io import save_object
 from utils.timer import Timer
 
@@ -291,7 +292,9 @@ def test_net(
             box_proposals = None
 
         timers['im_load'].tic()
-        im = entry['dataset'].load_image(entry)
+        im = entry['dataset'].load_sequence(
+            entry, cfg.DATA_LOADER.NUM_STACKED_FRAMES)
+        im = pack_sequence(im)
         timers['im_load'].toc()
         cls_boxes_i, cls_segms_i, cls_keyps_i = im_detect_all(model, im, box_proposals, timers)
 

commit 8061fb0ea951e19b4a08ac5dc0569511f1037d2d

Critical mean subtraction bugfix
Commit 88b4809f32261d8eed4367d95f71473818308808 introduced a bug that
led to pixel means not actually being subtracted from the input images.
This wasn't a problem for flow models, but was indeed a problem for RGB
images!

I have went back and checked any results that may have used this buggy
code and removed it.

lib/utils/blob.py
@@ -122,8 +122,7 @@ def prep_im_for_blob(im, pixel_means, target_sizes, max_size):
     the scale factors that were used to compute each returned image.
     """
     im = im.astype(np.float32, copy=False)
-    im_list = unpack_sequence(im)
-    im = pack_sequence([x - pixel_means for x in im_list])
+    im = pack_sequence([x - pixel_means for x in unpack_sequence(im)])
     im_shape = im.shape
     im_size_min = np.min(im_shape[0:2])
     im_size_max = np.max(im_shape[0:2])
@@ -139,7 +138,7 @@ def prep_im_for_blob(im, pixel_means, target_sizes, max_size):
                 None,
                 fx=im_scale,
                 fy=im_scale,
-                interpolation=cv2.INTER_LINEAR) for x in im_list
+                interpolation=cv2.INTER_LINEAR) for x in unpack_sequence(im)
         ]
         ims.append(pack_sequence(im_resized))
         im_scales.append(im_scale)