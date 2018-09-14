db709295a000716cbf324ee1948a352ef4afa1fa
Achal Dave <achalddave@live.com>
2018-09-14 10:16:55 -0400

    Always visualize at threshold=0.7

--- tools/visualize_json.py
+++ tools/visualize_json.py
@@ -114,7 +114,7 @@ def main():
             dataset=dataset,
             box_alpha=0.3,
             show_class=True,
-            thresh=0.5,
+            thresh=0.7,
             kp_thresh=2,
             dpi=300,
             ext='png')
