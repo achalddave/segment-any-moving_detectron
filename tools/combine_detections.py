ed9fa4e7098d4584586f2c82110fbfaddf2608da
Achal Dave <achalddave@live.com>
2018-09-21 11:13:03 -0400

    More options for combining detections

--- tools/combine_detections.py
+++ tools/combine_detections.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 import numpy as np
 import pickle
+import pprint
 from copy import deepcopy
 from pathlib import Path
 
@@ -11,6 +12,42 @@ import pycocotools.mask as mask_util
 
 import _init_paths  # noqa: F401
 from utils.logging import setup_logging
+
+
+def translate_range(value, old_range, new_range):
+    """
+    Translate value in one range to another. Useful for scaling scores.
+
+    >>> translate_range(0.5, (0, 1), (0, 2))
+    1.0
+    >>> translate_range(1, (0, 1), (0, 2))
+    2.0
+    >>> translate_range(3, (2, 4), (5, 6))
+    5.5
+    >>> translate_range(0.5, (0, 1), (0, 2))
+    1.0
+    >>> translate_range(np.array([2, 2.5, 3, 3.5, 4]), (2, 4), (5, 7)).tolist()
+    [5.0, 5.5, 6.0, 6.5, 7.0]
+    >>> translate_range(np.array([2, 2.5, 3, 3.5, 4]), (2, 4), (5, 5)).tolist()
+    [5.0, 5.0, 5.0, 5.0, 5.0]
+    """
+    value = np.asarray(value)
+    old_min, old_max = old_range
+
+    if np.any(value < old_min):
+        raise ValueError('Value(s) (%s) < min(old_range) (%s)' %
+                         (value[value < old_min], old_min))
+
+    if np.any(value > old_max):
+        raise ValueError('Value(s) (%s) > max(old_range) (%s)' %
+                         (value[value > old_max], old_max))
+
+    if (old_max - old_min) < 1e-10:
+        return old_max
+
+    new_min, new_max = new_range
+    scale = (new_max - new_min) / (old_max - old_min)
+    return (value - old_min) * scale + new_min
 
 
 class Result:
@@ -167,6 +204,102 @@ def merge_second_after_first(first, second):
     return merge(first, second)
 
 
+def merge_overlap1_nonoverlap1(first, second):
+    """
+    Output only detections from first, moving non-overlapping detections last.
+
+    The ranking is ordered as follows:
+        - detections from first that overlap with second
+        - detections from first that don't overlap with second
+    """
+    assert first.num_classes == second.num_classes
+    assert first.num_images == second.num_images
+    num_classes = first.num_classes
+    num_images = first.num_images
+
+    output_results = Result.empty_result(num_classes, num_images)
+    min_score = float('inf')
+    max_score = float('-inf')
+
+    for c in range(num_classes):
+        for i in range(num_images):
+            if len(first.boxes[c][i]) > 0:
+                first_scores = first.boxes[c][i][:, 4]
+                min_score = min(min_score, first_scores.min())
+                max_score = max(max_score, first_scores.max())
+
+    assert np.isfinite(min_score)
+    assert np.isfinite(max_score)
+
+    # Split range of [min_score, max_score] into two ranges:
+    # [min_score, mid_score), [mid_score, max_score]. The former range will
+    # contain non-overlapping detections, while the latter contains overlapping
+    # detections.
+    mid_score = (max_score + min_score) / 2
+
+    IOU_THRESH = 0.8
+    for c in range(num_classes):
+        for i in range(num_images):
+            masks1 = first.masks[c][i]
+            masks2 = second.masks[c][i]
+
+            if not masks2 or not masks1:
+                output_results.boxes[c][i] = first.boxes[c][i]
+                output_results.masks[c][i] = first.masks[c][i]
+                output_results.keypoints[c][i] = first.keypoints[c][i]
+                continue
+
+            # (num_masks1, num_masks2)
+            ious = mask_util.iou(
+                masks1, masks2, pyiscrowd=np.zeros(len(second.masks)))
+
+            # (num_masks1,) array
+            overlapping_first_binary = np.any(ious > IOU_THRESH, axis=1)
+            overlapping_first = np.where(overlapping_first_binary)[0]
+            nonoverlapping_first = np.where(~overlapping_first_binary)[0]
+
+            overlapping_boxes = first.boxes[c][i][overlapping_first]
+            nonoverlapping_boxes = first.boxes[c][i][nonoverlapping_first]
+
+            overlapping_boxes[:, 4] = translate_range(
+                overlapping_boxes[:, 4], (min_score, max_score),
+                (mid_score, max_score))
+            nonoverlapping_boxes[:, 4] = translate_range(
+                nonoverlapping_boxes[:, 4], (min_score, max_score),
+                (min_score, mid_score))
+
+            overlapping_masks = [
+                first.masks[c][i][m] for m in overlapping_first
+            ]
+            nonoverlapping_masks = [
+                first.masks[c][i][m] for m in nonoverlapping_first
+            ]
+
+            if first.keypoints[c][i]:
+                overlapping_keypoints = [
+                    first.keypoints[c][i][m] for m in overlapping_first
+                ]
+                nonoverlapping_keypoints = [
+                    first.keypoints[c][i][m] for m in nonoverlapping_first
+                ]
+            else:
+                overlapping_keypoints = []
+                nonoverlapping_keypoints = []
+
+            if (len(overlapping_masks) + len(nonoverlapping_masks) !=
+                    first.boxes[c][i].shape[0]):
+                __import__('ipdb').set_trace()
+            output_results.boxes[c][i] = np.vstack((overlapping_boxes,
+                                                    nonoverlapping_boxes))
+            output_results.masks[c][i] = (
+                overlapping_masks + nonoverlapping_masks)
+            output_results.keypoints[c][i] = (
+                overlapping_keypoints + nonoverlapping_keypoints)
+            assert (output_results.boxes[c][i].shape[0] == len(
+                output_results.masks[c][i]))
+    return output_results
+
+
 def main():
     with open(__file__, 'r') as f:
         _source = f.read()
@@ -174,25 +307,38 @@ def main():
     # Use first line of file docstring as description if it exists.
     parser = argparse.ArgumentParser(
         description=__doc__.split('\n')[0] if __doc__ else '',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument(
         '--detections1',
         help='detection pickle file',
         required=True)
     parser.add_argument(
         '--detections2',
-        help='detection pickle file)',
+        help='detection pickle file',
         required=True)
     parser.add_argument('--output-dir', required=True)
     parser.add_argument(
         '--method',
         default='merge',
-        choices=['merge', 'non-overlapping', 'second-after-first'],
-        help="""Method to combine detections. [merge]: Output all detections
-        from both detection files. [non-overlapping]: Keep all detections from
-        eval1, and detections from eval2 that don't overlap with eval1.
-        [second-after-first]: Reduce score of second predictions to be below
-        that of first predictions per image.""")
+        choices=[
+            'all1+all2', 'all1+nonoverlap2', 'overlap1>nonoverlap1',
+            'overlap1>nonoverlap1>nonoverlap2', 'all1>all2'
+        ],
+        help=('Method to combine detections.\n'
+              """[Selection]: "all" indicates all detections. "overlap1"
+              indicates detections from detections1 that overlap with a
+              detection from detections2.  "nonoverlap1" = all1 - overlap1."""
+              '\n'
+              """[Combination]: "+" indicates that the two detections are
+              concatenated into a single list, keeping the original scores.
+              "x>y" indicates that x and y are concatenated, with x set to rank
+              above y."""
+              '\n'
+              """Putting this all together, "overlap1>nonoverlap1>nonoverlap2"
+              ranks detections from detections1 that overlap with detections2
+              highest; then, detections from detections1 that don't overlap
+              with detections2; then, detections detections2 that don't overlap
+              with detections1."""))
 
     args = parser.parse_args()
 
@@ -202,7 +348,7 @@ def main():
     logging_path = str(output_dir / (Path(__file__).stem + '.py.log'))
     setup_logging(logging_path)
 
-    logging.info('Args:\n%s' % args)
+    logging.info('Args:\n%s' % pprint.pformat(vars(args)))
 
     with open(args.detections1, 'rb') as f:
         result1 = Result.from_file(f)
@@ -210,12 +356,14 @@ def main():
     with open(args.detections2, 'rb') as f:
         result2 = Result.from_file(f)
 
-    if args.method == 'merge':
+    if args.method == 'all1+all2':
         merged_result = merge(result1, result2)
-    elif args.method == 'non-overlapping':
+    elif args.method == 'all1+nonoverlap2':
         merged_result = merge_non_overlapping(result1, result2)
-    elif args.method == 'second-after-first':
+    elif args.method == 'all1>all2':
         merged_result = merge_second_after_first(result1, result2)
+    elif args.method == 'overlap1>nonoverlap1':
+        merged_result = merge_overlap1_nonoverlap1(result1, result2)
     else:
         raise NotImplementedError
 
