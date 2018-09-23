1835baf3f6d23907c189166eb5e392910c8d46a3
Achal Dave <achalddave@live.com>
2018-09-23 17:11:56 -0400

    datasets: Add YTVOS flownet2 datasets

--- lib/datasets/dataset_catalog.py
+++ lib/datasets/dataset_catalog.py
@@ -649,6 +649,48 @@ DATASETS = {
         IS_FLOW: True,
         IMAGE_EXTENSION: '.png'
     },
+    'ytvos_flownet2_sub_train_8-21-18': {
+        IM_DIR: _DATA_DIR + '/ytvos/flownet2/train',
+        ANN_FN: _DATA_DIR + '/ytvos/train-splits/sub-train-no-last-frame.json',
+        NUM_CLASSES: 2,
+        IS_FLOW: True,
+        IMAGE_EXTENSION: '.png'
+    },
+    'ytvos_flownet2_all_moving_sub_train_8-21-18': {
+        IM_DIR: _DATA_DIR + '/ytvos/flownet2/train',
+        ANN_FN: _DATA_DIR + '/ytvos/all-moving-8-21-18/sub-train-no-last-frame.json',
+        NUM_CLASSES: 2,
+        IS_FLOW: True,
+        IMAGE_EXTENSION: '.png'
+    },
+    'ytvos_flownet2_all_moving_sub_val_8-21-18': {
+        IM_DIR: _DATA_DIR + '/ytvos/flownet2/train',
+        ANN_FN: _DATA_DIR + '/ytvos/all-moving-8-21-18/sub-val-no-last-frame.json',
+        NUM_CLASSES: 2,
+        IS_FLOW: True,
+        IMAGE_EXTENSION: '.png'
+    },
+    'ytvos_flownet2_all_moving_strict_sub_train_8-21-18': {
+        IM_DIR: _DATA_DIR + '/ytvos/flownet2/train',
+        ANN_FN: _DATA_DIR + '/ytvos/all-moving-8-21-18/strict/sub-train-no-last-frame.json',
+        NUM_CLASSES: 2,
+        IS_FLOW: True,
+        IMAGE_EXTENSION: '.png'
+    },
+    'ytvos_flownet2_all_moving_strict_sub_val_8-21-18': {
+        IM_DIR: _DATA_DIR + '/ytvos/flownet2/train',
+        ANN_FN: _DATA_DIR + '/ytvos/all-moving-8-21-18/strict/sub-val-no-last-frame.json',
+        NUM_CLASSES: 2,
+        IS_FLOW: True,
+        IMAGE_EXTENSION: '.png'
+    },
+    'ytvos_flownet2_all_moving_strict_interesting_sub_val_8-21-18': {
+        IM_DIR: _DATA_DIR + '/ytvos/flownet2/train',
+        ANN_FN: _DATA_DIR + '/ytvos/all-moving-8-21-18/strict/interesting-only/sub-val-no-last-frame.json',
+        NUM_CLASSES: 2,
+        IS_FLOW: True,
+        IMAGE_EXTENSION: '.png'
+    },
     'keypoints_coco_2014_train': {
         IM_DIR:
             _DATA_DIR + '/coco/images/train2014',
